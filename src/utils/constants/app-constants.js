052d1bf6256849474bad3d9f61bdfaa34b54ff54
henryazer@outlook.com <henryazer@outlook.com>
2024-01-30 11:36:55 +0200

    update base url dev to mock server

--- src/utils/constants/app-constants.js
+++ src/utils/constants/app-constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = {
-  DEV: 'http://127.0.0.1:8080',
+  DEV: 'https://af2c2898-e151-4c53-84d4-9b4deee7e98e.mock.pstmn.io',
   STAGING: 'http://127.0.0.1:8080',
   PRODUCTION: 'http://127.0.0.1:8080'
 }
